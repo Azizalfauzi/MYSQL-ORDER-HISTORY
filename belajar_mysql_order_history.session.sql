3ee9f1fdf61a4a6030dbeb3bfe7cb267b8f71a9d
Aziz Alfauzi <azizalfauzi@student.telkomuniversity.ac.id>
2023-11-16 14:11:35 +0700

    * MYSQL-ORDER-HISTORY - Insert data in orders and order_details

--- belajar_mysql_order_history.session.sql
+++ belajar_mysql_order_history.session.sql
@@ -20,6 +20,9 @@ CREATE TABLE orders(
     total_amount BIGINT NOT NULL
 ) Engine InnoDb;
 ----
+ALTER TABLE orders
+ADD COLUMN created_at TIMESTAMP NOT NULL;
+----
 SELECT *
 FROM orders;
 ----
@@ -35,4 +38,137 @@ CREATE TABLE order_details(
     FOREIGN KEY fk_orders_to_order_details (order_id) REFERENCES orders(id)
 ) Engine InnoDb;
 ---
-SELECT * FROM order_details;
+SELECT *
+FROM order_details;
+--- 
+INSERT INTO orders (
+        id,
+        created_at,
+        seller_id,
+        seller_name,
+        buyer_id,
+        buyer_name,
+        shipping_name,
+        shipping_address,
+        shipping_phone,
+        logistic_id,
+        logistic_name,
+        payment_method_id,
+        payment_method_name,
+        total_quantity,
+        total_weight,
+        total_product_amount,
+        total_shipping_cost,
+        total_shopping_amount,
+        service_charge,
+        total_amount
+    )
+VALUES(
+        '122',
+        NOW(),
+        'galery_olahraga_indonesia',
+        'Galery Olahraga Indonesia',
+        'zuha_123',
+        'Aziz Alfa',
+        'Budi Nugraha',
+        'jl.Papandayan,Ds Kauman,Kec Kauman,66261',
+        '082211',
+        'Sicepat',
+        'Sicepat-Gokil',
+        'debit_online',
+        'Debit Online',
+        5,
+        5360,
+        508500,
+        60000,
+        568500,
+        1000,
+        569500
+    );
+---
+SELECT *
+FROM orders;
+---
+INSERT INTO order_details(
+        id,
+        order_id,
+        product_id,
+        product_name,
+        product_weight,
+        product_price,
+        quantity,
+        total_amount
+    )
+VALUES(
+        '1',
+        '122',
+        'P1',
+        'BasketBall',
+        2300,
+        177900,
+        2,
+        355800
+    );
+---
+INSERT INTO order_details(
+        id,
+        order_id,
+        product_id,
+        product_name,
+        product_weight,
+        product_price,
+        quantity,
+        total_amount
+    )
+VALUES(
+        '2',
+        '122',
+        'P2',
+        'BasketBall-Red',
+        500,
+        98000,
+        1,
+        98000
+    );
+---
+INSERT INTO order_details(
+        id,
+        order_id,
+        product_id,
+        product_name,
+        product_weight,
+        product_price,
+        quantity,
+        total_amount
+    )
+VALUES(
+        '3',
+        '122',
+        'P3',
+        'Pentil Pompa Bola',
+        54,
+        9900,
+        1,
+        9900
+    );
+---
+INSERT INTO order_details(
+        id,
+        order_id,
+        product_id,
+        product_name,
+        product_weight,
+        product_price,
+        quantity,
+        total_amount
+    )
+VALUES(
+        '4',
+        '122',
+        'P4',
+        'Pompa Bola',
+        198,
+        43900,
+        1,
+        43900
+    );
